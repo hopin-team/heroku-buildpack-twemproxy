f56ccb85881467b0f30df5a2cb0615bc19c46ffc
Bruno Andrade <bruno@hopin.to>
2020-05-19 23:01:10 +0100

    update gen for redis tls (#2)
    * modify gen-twemproxy-conf and start-twemproxy
    
    * Add the ability to check protocol redis or rediss

--- bin/gen-twemproxy-conf.sh
+++ bin/gen-twemproxy-conf.sh
@@ -5,14 +5,15 @@ rm -f /app/vendor/twemproxy/twemproxy.yml
 echo "Setting REDIS_URL_TWENPROXY config var"
 eval REDIS_URL_VALUE="${REDIS_URL}"
 
-DB=$(echo "${REDIS_URL_VALUE}" | perl -lne 'print "$1 $2 $3 $4 $5 $6" if /^rediss?(?:ql)?:\/\/([^:]+):([^@]+)@(.*?):(.*?)(\\?.*)?$/')
+DB=$(echo "${REDIS_URL_VALUE}" | perl -lne 'print "$1 $2 $3 $4 $5 $6" if /^(rediss?)(?:ql)?:\/\/([^:]+):([^@]+)@(.*?):(.*?)(\\?.*)?$/')
 DB_URI=( $DB )
-DB_USER=${DB_URI[0]}
-DB_PASS=${DB_URI[1]}
-DB_HOST=${DB_URI[2]}
-DB_PORT=${DB_URI[3]}
+DB_PROTO=${DB_URI[0]}
+DB_USER=${DB_URI[1]}
+DB_PASS=${DB_URI[2]}
+DB_HOST=${DB_URI[3]}
+DB_PORT=${DB_URI[4]}
 
-NEW_URL=rediss://${DB_USER}:${DB_PASS}@127.0.0.1:6201
+NEW_URL=${DB_PROTO}://${DB_USER}:${DB_PASS}@127.0.0.1:6201
 export REDIS_URL_TWEMPROXY=${NEW_URL}
 
 echo "Pointing to ${DB_HOST}:${DB_PORT}"
