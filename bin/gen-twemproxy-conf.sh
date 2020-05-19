349cdf4fa8e2cb5e394b24914c3215a352dc0879
Bruno Andrade <bruno@hopin.to>
2020-05-19 21:11:06 +0100

    modify gen-twemproxy-conf and start-twemproxy (#1)

--- bin/gen-twemproxy-conf.sh
+++ bin/gen-twemproxy-conf.sh
@@ -1,36 +1,30 @@
 #!/usr/bin/env bash
 
-REDIS_URLS=${TWEMPROXY_URLS:-REDISCLOUD_URL}
-n=1
-
 rm -f /app/vendor/twemproxy/twemproxy.yml
 
-for REDIS_URL in $REDIS_URLS
-do
-  echo "Setting ${REDIS_URL}_TWEMPROXY config var"
-  eval REDIS_URL_VALUE=\$$REDIS_URL
+echo "Setting REDIS_URL_TWENPROXY config var"
+eval REDIS_URL_VALUE="${REDIS_URL}"
 
-  DB=$(echo ${REDIS_URL_VALUE} | perl -lne 'print "$1 $2 $3 $4 $5 $6" if /^redis(?:ql)?:\/\/([^:]+):([^@]+)@(.*?):(.*?)(\\?.*)?$/')
-  DB_URI=( $DB )
-  DB_USER=${DB_URI[0]}
-  DB_PASS=${DB_URI[1]}
-  DB_HOST=${DB_URI[2]}
-  DB_PORT=${DB_URI[3]}
+DB=$(echo "${REDIS_URL_VALUE}" | perl -lne 'print "$1 $2 $3 $4 $5 $6" if /^rediss?(?:ql)?:\/\/([^:]+):([^@]+)@(.*?):(.*?)(\\?.*)?$/')
+DB_URI=( $DB )
+DB_USER=${DB_URI[0]}
+DB_PASS=${DB_URI[1]}
+DB_HOST=${DB_URI[2]}
+DB_PORT=${DB_URI[3]}
 
-  NEW_URL=redis://${DB_USER}:${DB_PASS}@127.0.0.1:620${n}
-  export ${REDIS_URL}_TWEMPROXY=${NEW_URL}
-  echo "Pointing to ${DB_HOST}:${DB_PORT}"
+NEW_URL=rediss://${DB_USER}:${DB_PASS}@127.0.0.1:6201
+export REDIS_URL_TWEMPROXY=${NEW_URL}
 
-  cat >> /app/vendor/twemproxy/twemproxy.yml << EOFEOF
+echo "Pointing to ${DB_HOST}:${DB_PORT}"
+
+cat >> /app/vendor/twemproxy/twemproxy.yml << EOFEOF
 ${REDIS_URL}:
-  listen: 127.0.0.1:620${n}
+  listen: 127.0.0.1:6201
   redis: true
   redis_auth: ${DB_PASS}
+  timeout: 30000
   servers:
    - ${DB_HOST}:${DB_PORT}:1
 EOFEOF
-
-  let "n += 1"
-done
 
 chmod go-rwx /app/vendor/twemproxy/*
